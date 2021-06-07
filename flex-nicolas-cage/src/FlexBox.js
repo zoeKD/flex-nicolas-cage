be418480c0fc632aca31be1d2783a33281ceac0d
Zoé Cadé <zoe@talent.io>
2021-06-07 21:01:21 +0200

    completed exercise

--- flex-nicolas-cage/src/FlexBox.js
+++ flex-nicolas-cage/src/FlexBox.js
@@ -1,17 +1,46 @@
 import styled, { css } from 'styled-components'
 
 
-const styleStep1 = css``
-const styleStep2 = css``
-const styleStep3 = css``
-const styleStep4 = css``
-const styleStep5 = css``
-const styleStep6 = css``
-const styleStep7 = css``
-const styleStep8 = css``
-const styleStep9 = css``
+const styleStep1 = css`
+  justify-content: center;
+`
+const styleStep2 = css`
+  justify-content: space-between;
+  align-items: flex-end;
+`
+const styleStep3 = css`
+  justify-content: space-around;
+  align-items: flex-end;
+`
+const styleStep4 = css`
+  justify-content: center;
+  align-items: flex-end;
+`
+const styleStep5 = css`
+  border-bottom: 3px solid brown;
+`
+const styleStep6 = css`
+  justify-content: space-between;
+  align-items: flex-end;
+  border-bottom: 3px solid brown;
+`
+const styleStep7 = css`
+  justify-content: space-between;
+  align-items: center;
+`
+const styleStep8 = css`
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3rem;
+`
+const styleStep9 = css`
+  flex-wrap: wrap;
+  width: 85%
+`
 const styleStep10 = css``
-const styleStep11 = css``
+const styleStep11 = css`
+  justify-content: center;
+`
 const styleStep12 = css``
 
 
@@ -22,19 +51,36 @@ const Container = styled.div`
 `
 
 const NicolasCage = styled.div`
+  display: flex;
+  flex-direction: column;
+  height:  ${({ step }) => step >= 10 && '60vh'};
+  order: ${({ step }) => step === 9 && '3'};
+`
+
+const Saphira = styled.img`
+  height: ${({ step }) => step === 11 ? '2px' : '20%'};
+`
+
+const Fire = styled.img`
+  transform: rotate(45deg);
+  height: ${({ step }) => [8, 9].includes(step) && '30%'};
+`
+
+const Body = styled.img`
+  flex-grow: ${({ step }) => step >= 10 && '1'};
 `
 
 const FlexBox = ({ step }) => (
   <Container step={step}>
     <NicolasCage step={step}>
       <img src="/nicolas-cage.png" alt="nicolas head" height="100px" />
-      <img src="/body.png" alt="nicolas body" height="50px" />
+      <Body src="/body.png" alt="nicolas body" height="50px" step={step} />
       <img src="/feet.png" alt="nicolas feet" height="50px" />
     </NicolasCage>
 
     {[2, 3, 4].includes(step) && <img src="/forest.png" alt="forest" height="100px" />}
-    {[7, 8, 9].includes(step) && <img src="/fire.png" alt="forest" height="100px" />}
-    {step > 5 && step < 12 && <img src="/dragon.png" alt="forest" height="100px" />}
+    {[7, 8, 9].includes(step) && <Fire src="/fire.png" alt="fire" height="100px" step={step} />}
+    {step > 5 && step < 12 && <Saphira src="/dragon.png" alt="forest" step={step} />}
   </Container>
 )
 
